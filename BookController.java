ee40d1e22d92917efadb85d36f3ea847c863dd6a
codingzzang123 <104754061+codingzzang123@users.noreply.github.com>
2022-06-24 00:57:20 +0900

    Paging
    페이징 추가 완료

---
+++ BookController.java
@@ -0,0 +1,141 @@
+package com.hosun.web.book.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.hosun.web.book.model.BookVO;
+import com.hosun.web.book.service.BookService;
+
+@Controller
+public class BookController {
+	
+	@Autowired
+	private BookService service;
+	@Autowired
+	private Paging p;
+	
+	private PagingCommand command;
+	private static final DecimalFormat f = new DecimalFormat("###,###,###");
+	
+	@RequestMapping("/books/list") 
+	public String form(HttpServletRequest request, Model model) {
+		
+		command = new PagingCommand();
+		String pageNum = request.getParameter("pageNum");
+		String search = request.getParameter("search"); // 책이름 or 저자
+		String keyword = request.getParameter("keyword"); // input에 친 내용
+		
+		if(pageNum == null){
+			pageNum = "1";
+	    }
+		int PageSize = 5; //한 화면에 보여지는 게시물의 갯수 
+	    int curPageNum = Integer.parseInt(pageNum);
+	    int lastPageNum;
+	    int start = (curPageNum-1) * PageSize + 1;
+	    int end = curPageNum * PageSize;
+	    
+		List<BookVO> ls = new ArrayList<>();
+
+		if(search==null||search.length()==0) { //검색하지 않았을때
+			command.setStart(start); command.setEnd(end); 
+			ls = service.list(command);
+			p.makeLastPageNum(); 
+		}
+		else if(search.equals("title")) { //책 제목 검색
+			command.setStart(start); command.setEnd(end); command.setBookname(keyword);
+			ls = service.searchBookname(command);
+			p.makeLastPageNumTitle(keyword);
+		}
+		else if(search.equals("writer")) { //글쓴이 검색
+			command.setStart(start); command.setEnd(end); command.setWriter(keyword);
+			ls = service.searchWriter(command);
+			p.makeLastPageNumWriter(keyword);
+		}
+		
+		lastPageNum = p.getLastPageNum();
+		
+		p.makeBlock(curPageNum);
+	    Integer blockStartNum = p.getBlockStartNum();
+	    Integer blockLastNum = p.getBlockLastNum();
+	    
+	    /* 페이징 attribute 추가 */
+	    model.addAttribute("blockStartNum",blockStartNum); //블럭 시작 넘버 ,
+	    model.addAttribute("blockLastNum",blockLastNum); //추가한것1.
+	    model.addAttribute("now",curPageNum); //현재 페이지 위치 
+	    model.addAttribute("end",lastPageNum); //블럭 마지막 번호
+	    model.addAttribute("search",search); 
+	    model.addAttribute("keyword",keyword);
+	    
+		model.addAttribute("ls",ls);
+		return "book/list";
+	}
+	
+	@GetMapping(value="/books/add")
+	public String form(WriteForm writeForm,Model model) {
+		if(writeForm==null) {
+			model.addAttribute("writeForm",new WriteForm());
+		}
+		return "book/write";
+	}
+	
+	
+	@PostMapping(value="/books/add")
+	public String submit(@Valid WriteForm writeForm, BindingResult bindingResult,Model model,HttpServletRequest req) throws IllegalStateException, IOException{
+		if(bindingResult.hasErrors()) {
+			model.addAttribute("writeForm",writeForm);
+			return "book/write";
+		}
+		MultipartFile uploadFile = writeForm.getImg();
+		if(!uploadFile.isEmpty()) {
+			
+			uploadFile.transferTo(new File(req.getServletContext().getRealPath("/resources/bookImg")
+					,uploadFile.getOriginalFilename()));
+			
+			
+			int priceB = Integer.parseInt(writeForm.getPrice());
+			BookVO vo = new BookVO(writeForm.getbookname(),writeForm.getWriter(),
+					writeForm.getPublisher(),f.format(priceB),uploadFile.getOriginalFilename(),
+					writeForm.getInfo(),Integer.parseInt(writeForm.getIsbn()));
+			service.write(vo);
+			
+			return "redirect:/books/list";
+		}else {
+			model.addAttribute("writeForm",writeForm);
+			return "book/write";
+		}
+	}
+	
+	
+	@PostMapping("/books/makeIsbn")
+	@ResponseBody
+	public long idCheck() {
+		return System.currentTimeMillis()/1000;
+	}
+	
+	@RequestMapping("/books/detail/{no}")
+	public String detail(@PathVariable("no")int bookId,Model model) {
+		BookVO ls = service.listOne(bookId);
+		model.addAttribute("ls",ls);
+		return "book/detail";
+	}
+	
+}
---
+++ Paging.java
@@ -0,0 +1,103 @@
+package com.hosun.web.book.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.hosun.web.book.model.BookDAO;
+
+@Component
+public class Paging {
+	
+	@Autowired
+	private BookDAO dao;
+	
+	private final static int pageBlock = 5; // (1~5) (6~10) 같이 블럭의 갯수.
+    private int blockStartNum = 0;
+    private int blockLastNum = 0;
+    private int lastPageNum = 0;
+    
+    public int getPageBlock() {
+    	return pageBlock;
+    }
+    public int getBlockStartNum() {
+        return blockStartNum;
+    }
+    public void setBlockStartNum(int blockStartNum) {
+        this.blockStartNum = blockStartNum;
+    }
+    public int getBlockLastNum() {
+        return blockLastNum;
+    }
+    public void setBlockLastNum(int blockLastNum) {
+        this.blockLastNum = blockLastNum;
+    }
+    public int getLastPageNum() {
+        return lastPageNum;
+    }
+    public void setLastPageNum(int lastPageNum) {
+        this.lastPageNum = lastPageNum;
+    }
+   
+    // block을 생성, 현재 페이지가 속한 block의 시작 번호, 끝 번호를 계산
+    public void makeBlock(int curPage){
+    	if(curPage-3 <= 0) {
+    		blockStartNum = 1;
+        	blockLastNum = (lastPageNum < 5 ? lastPageNum : 5);
+    	}else if(lastPageNum-curPage==1) {
+    		blockStartNum = curPage-3;
+    		blockLastNum = (curPage != lastPageNum? curPage+1 : curPage);
+    	}else if(lastPageNum-curPage==0) {
+    		blockStartNum = curPage-4;
+    		blockLastNum = lastPageNum;
+    	}else {
+    		blockStartNum = curPage-2;
+    		blockLastNum = (curPage != lastPageNum? curPage+2 : curPage);
+    	}
+    }
+//    public void makeBlock(int curPage){
+//        int blockNum = 0;
+//
+//        blockNum = (int)Math.floor((curPage-1)/ pageBlock);
+//        blockStartNum = (pageBlock * blockNum) + 1;
+//        
+//        blockLastNum = blockStartNum + (pageBlock-1);
+//        
+//        if(blockLastNum / lastPageNum > 0)
+//        	blockLastNum = lastPageNum;
+//    }
+
+	// 총 페이지의 마지막 번호(검색 하지 않았을때 + 검색 했을 때)
+    public void makeLastPageNum() {
+    	int total = dao.getCount(); 
+
+        if( total % pageBlock == 0 ) {
+            lastPageNum = (int)Math.floor(total/5);
+        }
+        else {
+            lastPageNum = (int)Math.floor(total/5) + 1;
+        }
+    }
+    public void makeLastPageNumWriter(String ls) {
+    	int total = dao.getWriterCount(ls); 
+    	
+    	if( total % pageBlock == 0 ) {
+    		this.lastPageNum = (int)Math.floor(total/5);
+    	}
+    	else {
+    		lastPageNum = (int)Math.floor(total/5) + 1;
+    	}
+    }
+    
+    
+    public void makeLastPageNumTitle(String ls) {
+    	int total = dao.getTitleCount(ls);
+    	
+    	if( total % pageBlock == 0 ) {
+    		this.lastPageNum = (int)Math.floor(total/5);
+    	}
+    	else {
+    		lastPageNum = (int)Math.floor(total/5) + 1;
+    	}
+    }
+}
